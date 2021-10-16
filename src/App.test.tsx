4d9b3d19bc8aa659030c0874884b50774e070822
Bartosz Gościcki <s19677@pjwstk.edu.pl>
2021-10-16 13:45:11 +0200

    feat: remove unnecessary boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Will maybe come in handy later?
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,6 @@
+/*
+This file references TypeScript types declarations that are specific to projects started with Create React App.
+These type declarations add support for importing resource files such as bmp, gif, jpeg, jpg, png, webp, and svg. 
+That means that the following import will work as expected without errors: import logo from './logo.svg';
+*/
 /// <reference types="react-scripts" />

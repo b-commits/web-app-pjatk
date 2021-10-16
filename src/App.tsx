ac42ca2bdeae80dbee44d850a9c04c6db537b735
Bartosz Gościcki <s19677@pjwstk.edu.pl>
2021-10-16 14:31:52 +0200

    feat: add routing and a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { About } from './components/About/About';
 import { Home } from './components/Home/Home';
+import { Navbar } from './components/Navbar/Navbar';
 
 export const App = () => {
   return (
     <Router>
+      <Navbar />
       <div className="App">
         <Switch>
           <Route path="/" exact component={Home} />
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar: React.FC = () => {
-  return <div></div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink
+            className="navLink"
+            activeClassName="activeNavLink"
+            exact
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="navLink"
+            activeClassName="activeNavLink"
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };

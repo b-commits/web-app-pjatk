cbd2b50507901f4deff62a39a836aea295dc820e
Rafał Sadowski <riderbrand@gmail.com>
2022-01-05 18:59:01 +0100

    feat: Add footer content and styles

--- src/components/Footer/Footer.style.tsx
+++ src/components/Footer/Footer.style.tsx
@@ -1,11 +1,93 @@
 import { css } from '@emotion/react';
 
 export const footerContainer = css({
+  fontFamily: 'Roboto',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  minHeight: '150px',
+  marginTop: '50px',
+  marginBottom: '0px',
+  padding: '50px',
+  backgroundColor: 'rgba(255, 255, 255, 1)',
+  boxShadow: '3px 3px 10px 1px rgba(55, 58, 54, 1)',
+  boxSizing: 'border-box',
+});
+
+export const footerTitle = css({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  marginTop: '30px',
-  width: '100%',
-  height: '150px',
-  backgroundColor: '#FFFFFF',
+  fontSize: '1.1em',
+});
+
+export const footerTitleImg = css({
+  height: '80px',
+});
+
+export const footerLinks = css({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  minWidth: '20%',
+  fontSize: '1em',
+  marginTop: '10px',
+});
+
+export const footerLink = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '30px',
+  color: '#ffffff',
+  textDecoration: 'none',
+  marginTop: '10px',
+  marginLeft: '5px',
+  marginRight: '5px',
+  width: '50px',
+  height: '50px',
+  padding: '5px',
+  borderRadius: '50%',
+  transition: '2s',
+  backgroundColor: 'rgb(37,31,255)',
+  background:
+    'linear-gradient(270deg, rgba(37,31,255,1) 0%, rgba(78,74,198,1) 100%, rgba(0,212,255,1) 100%)',
+  '&:hover': {
+    transform: 'scale(1.1)',
+  },
+});
+
+export const footerCopyrights = css({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '0.9em',
+  marginTop: '25px',
+});
+export const footerCopyrightsAndCopy = css({
+  color: '#aeaeae',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '0.8em',
+  marginTop: '10px',
+});
+
+export const footerCopyrightsAuthor = css({
+  marginTop: '10px',
+});
+
+export const footerCopyrightsAuthorLink = css({
+  color: '#000000',
+  marginLeft: '10px',
+  marginBottom: '10px',
+  textDecoration: 'none',
+  '&:hover ': {
+    color: 'rgba(78,74,198,1)',
+  },
 });
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,71 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
-import { footerContainer } from './Footer.style';
+import {
+  footerContainer,
+  footerTitle,
+  footerTitleImg,
+  footerLinks,
+  footerLink,
+  footerCopyrights,
+  footerCopyrightsAuthor,
+  footerCopyrightsAuthorLink,
+  footerCopyrightsAndCopy,
+} from './Footer.style';
+
+import logo from '../../logo.png';
 
 export const Footer: React.FC = () => {
-  return <footer css={footerContainer}> LFG APP</footer>;
+  return (
+    <footer css={footerContainer}>
+      <div css={footerTitle}>
+        <img css={footerTitleImg} src={logo} alt='logo' />
+      </div>
+      <div css={footerLinks}>
+        <a css={footerLink} href='https://github.com/b-commits/web-app-pjatk'>
+          <i className='fab fa-github-alt'></i>
+        </a>
+        <a css={footerLink} href='https://pl-pl.facebook.com/polskojaponska/'>
+          <i className='fab fa-facebook-f'></i>
+        </a>
+        <a css={footerLink} href='https://www.instagram.com/polskojaponska/'>
+          <i className='fab fa-instagram'></i>
+        </a>
+        <a css={footerLink} href='https://twitter.com/polskojaponska'>
+          <i className='fab fa-twitter'></i>
+        </a>
+        <a css={footerLink} href='https://www.youtube.com/user/TVPjwstk'>
+          <i className='fab fa-youtube'></i>
+        </a>
+      </div>
+      <div css={footerCopyrights}>
+        <div css={{ marginTop: '10px' }}>
+          <a css={footerCopyrightsAuthorLink} href='#'>
+            Terms of Use
+          </a>
+          <a css={footerCopyrightsAuthorLink} href='#'>
+            Privacy Policy
+          </a>
+        </div>
+        <span css={footerCopyrightsAuthor}>
+          <span>Authors:</span>
+          <a
+            css={footerCopyrightsAuthorLink}
+            href='https://github.com/b-commits'
+          >
+            b-commits
+          </a>
+          <a
+            css={footerCopyrightsAuthorLink}
+            href='https://github.com/blueversi'
+          >
+            blueversi
+          </a>
+          <a css={footerCopyrightsAuthorLink} href='https://github.com/3ciak'>
+            3ciak
+          </a>
+        </span>
+        <span css={footerCopyrightsAndCopy}>Copyright © 2022 LFG-APP</span>
+      </div>
+    </footer>
+  );
 };

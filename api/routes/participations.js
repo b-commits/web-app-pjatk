496559ebbd1a858d854a82e4469c2313bb1fdde3
b-commits <s19677@pjwstk.edu.pl>
2022-01-06 18:39:22 +0100

    fix: api should no longer accept duplicated participation records

--- api/routes/participations.js
+++ api/routes/participations.js
@@ -1,6 +1,4 @@
 const Participation = require('../models/Participation');
-const Listing = require('../models/Listing');
-const User = require('../models/User');
 const express = require('express');
 const { OK } = require('./errorConsts');
 const router = express.Router();
@@ -9,14 +7,44 @@ const router = express.Router();
     @route    GET /api/participations
     @desc     Get all users participating in a given listing.
     @access   Public.
+    @param    listingId - a listing for which to get a list of users
 */
 router.get('/', async (req, res, next) => {
   try {
     const participations = await Participation.query()
+      .join('user', {
+        'user.id': 'userId',
+      })
+      .where({
+        listingId: req.body.listingId,
+      })
+      .select('user.id', 'nickname');
+    res.status(200).json(participations);
+  } catch (err) {
+    res.status(400).json({ msg: err.message });
+  }
+});
+
+/** 
+    @route    GET /api/participations/perUser
+    @desc     Get listings a given user is participating in.
+    @access   Public.
+    @param    userId - the id of a user for which to get all listings
+              he is currently participating in 
+*/
+router.get('/perUser', async (req, res, next) => {
+  try {
+    const participations = await Participation.query()
+      .join('user', {
+        'user.id': 'userId',
+      })
+      .join('listing', {
+        'listing.id': 'listingId',
+      })
       .where({
         userId: req.body.userId,
       })
-      .select('id');
+      .select('listingId', 'message', 'listingGame');
     res.status(200).json(participations);
   } catch (err) {
     res.status(400).json({ msg: err.message });
@@ -30,10 +58,16 @@ router.get('/', async (req, res, next) => {
 */
 router.post('/', async (req, res, next) => {
   try {
-    await Participation.query().insert({
-      userId: req.body.userId,
+    const alreadyExists = await Participation.query().where({
+      userid: req.body.userId,
       listingId: req.body.listingId,
     });
+    if (alreadyExists.length <= 0) {
+      await Participation.query().insert({
+        userId: req.body.userId,
+        listingId: req.body.listingId,
+      });
+    }
     res.status(200).json(OK);
   } catch (err) {
     res.status(400).json({ msg: err.message });
